f3204d84e426432c6bf29cb8da6678838c1c5b58
pocari <caffelattenonsugar@gmail.com>
2020-05-02 12:11:53 +0900

    lint関連追加

---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: ['stylelint-order'],
+  extends: ['stylelint-config-standard', './node_modules/prettier-stylelint/config.js'],
+  ignoreFiles: ['**/node_modules/**'],
+  rules: {
+    indentation: 2,
+    'string-quotes': 'single',
+    'order/properties-alphabetical-order': true,
+  },
+};
---
+++ src/Home.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const Home: FC<{}> = () => {
+  return (
+    <>
+      <div>Home container</div>
+    </>
+  );
+};
+
+export default Home;
